1ef774bb859215ecb251e4dc3b61e5341df15a8f
Axel Laffite <laffite.axel81@gmail.com>
2021-12-19 20:23:48 +0100

    Section component #1  (#2)
    add: section component
    fix: setup tailwind properly

---
+++ frontend/components/DSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="section">
+        <slot></slot>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class DSection extends Vue{
+}
+</script>
+
+<style lang="scss" scoped>
+.section {
+    @apply p-6 min-w-full min-h-full;
+}
+</style>
