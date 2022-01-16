e66384f18734f7e922ed214a2b8012f89ee261c5
axel laffite <axel.laffite@univ-tlse3.fr>
2022-01-16 21:47:10 +0100

    testest

---
+++ frontend/components/DSlider.vue
@@ -0,0 +1,118 @@
+<template>
+    <div ref="cards" class="cards">
+        <slot></slot>
+        <div class="flex flex-row justify-between">
+            <d-button icon="arrow-left"></d-button>
+            <d-button icon="arrow-right"></d-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class DSlider extends Vue {
+    findCards(toHide: number, toShow: number): (HTMLElement | null)[] {
+        const cards = this.$refs["cards"] as HTMLElement
+        const cardToHide = cards.children.item(toHide) as HTMLElement | null
+        const cardToShow = cards.children.item(toShow) as HTMLElement | null
+
+        return [cardToHide, cardToShow]
+    }
+
+    showNextSlide(event: Event) {
+        const el = (event.target as HTMLElement | null)?.parentElement
+        const rawIndex = el?.dataset?.index ?? ""
+        const index = Number.parseInt(rawIndex)
+        if (isNaN(index)) {
+            return
+        }
+
+        const [cardToHide, cardToShow] = this.findCards(index, index + 1)
+        if (!cardToHide || !cardToShow) {
+            return
+        }
+
+        cardToHide.classList.add("hidden")
+        cardToShow.classList.add("shown")
+        cardToHide.classList.remove("shown")
+        cardToShow.classList.remove("hidden")
+    }
+
+    showPreviousSlide(event: Event) {
+        const el = (event.target as HTMLElement | null)?.parentElement
+        const rawIndex = el?.dataset?.index ?? ""
+        const index = Number.parseInt(rawIndex)
+        if (isNaN(index)) {
+            return
+        }
+
+        const [cardToHide, cardToShow] = this.findCards(index, index - 1)
+        if (!cardToHide || !cardToShow) {
+            return
+        }
+
+
+        cardToHide.classList.remove("shown")
+        cardToShow.classList.remove("hidden")
+        cardToHide.classList.add("hidden")
+        cardToShow.classList.add("shown")
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+@keyframes out-left {
+    0% { left: 50% }
+    100% { left: -100% }
+}
+
+@keyframes out-right {
+    0% { left: 50% }
+    100% { left: -100% }
+}
+
+@keyframes in-left {
+    0% { left: 50% }
+    100% { left: -100% }
+}
+
+@keyframes in-right {
+    0% { left: 50% }
+    100% { left: -100% }
+}
+
+
+.shown {
+    animation: 2s show-animation;
+}
+
+.hide {
+    animation: 2s hidden-animation;
+}
+
+.cards {
+    position: relative;
+}
+
+.card {
+    padding: 16px;
+    color: black;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    background-color: white;
+    width: 20em;
+    height: 20em;
+    transform: translateX(-50%) translateY(-50%);
+}
+
+button {
+    background: grey;
+    border-radius: 8px;
+    padding: 8px;
+}
+
+</style>
